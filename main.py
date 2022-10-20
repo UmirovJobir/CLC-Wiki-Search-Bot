0d239a5fd6165f8cd0d3cc81a1a7342741b554d2
Umirov Jobir <jobirumirovmoliya@gmail.com>
2022-10-20 18:05:33 +0500

    aaaa

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from urllib import response
 from telegram.bot import Bot
 from telegram.user import User
 from telegram.update import Update
 from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext
 import settings
+import requests  
 
 
 # bot = Bot(token="5552099898:AAGrWUcDRf0-Fg5sD5G6HZsS4EYZQLK-DYc")
@@ -10,14 +12,34 @@ import settings
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Salom')
-    context.bot.send_message(chat_id=update.message.chat_id, text='Salom yana bir bor')
+    update.message.reply_text("Assalomu alaykum! Vikipediadan ma'lumot qidiruvchi botga xush kelibsiz!" 
+                                "Ma'lumot izlash uchun /search va teks yozing. Misol uchun /search Amir Temur")
+    # context.bot.send_message(chat_id=update.message.chat_id, text='Salom yana bir bor')
 
+def search(update: Update, context: CallbackContext):
+    args = context.args
+    search_text = ' '.join(args)
+    response = requests.get('https://en.wikipedia.org/w/api.php',{
+        'action':'opensearch',
+        'search':search_text,
+        'limit':1,
+        'namespace':0,
+        'format':'json',
+    })
+    result = response.json()
+    link = result[3]
+
+    if len(link):
+        print(link[0])
+    else:
+        print('none')
 
 updater = Updater(token=settings.TOKEN)
 
 dipatcher = updater.dispatcher
 updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(CommandHandler("search", search))
+
 
 
 updater.start_polling()
